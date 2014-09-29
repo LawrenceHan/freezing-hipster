8ff901938100433a6d580fe3688809ee8fd25e11
LawrenceHan <Hanguang@LawrencetekiMacBook-Pro-2.local>
2014-09-29 09:54:45 +0800

    Created IOS8 scrollview demo

---
+++ IOS8_ScrollView/IOS8_ScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS8_ScrollView
+//
+//  Created by Hanguang on 9/29/14.
+//  Copyright (c) 2014 Hanguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS8_ScrollView/IOS8_ScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS8_ScrollView
+//
+//  Created by Hanguang on 9/29/14.
+//  Copyright (c) 2014 Hanguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
